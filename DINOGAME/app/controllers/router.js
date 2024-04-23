b0a4a7e8f860815d3ff2716ea38e36e36187f010
José Eduardo Pérez Valenzuela <136402555+PPrez24@users.noreply.github.com>
2024-04-23 14:35:21 -0600

    Create router.js

---
+++ DINOGAME/app/controllers/router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/index.html")));
+router.get('/index', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/index.html")));
+router.get('/login', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/login.html")));
+router.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/register.html")));
+router.get('/characters', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/characters.html")));
+router.get('/leaderboard', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/leaderboard.html")));
+router.get('/modificar', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/modificar.html")));
+
+module.exports = router;
